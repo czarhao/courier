f975902b8f12be97554ddf62485d12bbd9ba7707
root <root@pop-os.localdomain>
2020-12-31 15:13:07 +0800

    unix -> syscall

--- mount/pivot_root.go
+++ mount/pivot_root.go
@@ -2,41 +2,40 @@ package mount
 
 import (
 	"fmt"
-	"golang.org/x/sys/unix"
+	"syscall"
 )
 
 func pivotRoot(rootfs string) error {
-	oldroot, err := unix.Open("/", unix.O_DIRECTORY|unix.O_RDONLY, 0)
+	oldroot, err := syscall.Open("/", syscall.O_DIRECTORY|syscall.O_RDONLY, 0)
 	if err != nil {
 		return fmt.Errorf("get old rootfs failed, err: %v", err)
 	}
-	defer unix.Close(oldroot)
-	newroot, err := unix.Open(rootfs, unix.O_DIRECTORY|unix.O_RDONLY, 0)
+	defer syscall.Close(oldroot)
+	newroot, err := syscall.Open(rootfs, syscall.O_DIRECTORY|syscall.O_RDONLY, 0)
 	if err != nil {
 		return fmt.Errorf("get new rootfs failed, err: %v", err)
 	}
-	defer unix.Close(newroot)
+	defer syscall.Close(newroot)
 
-	if err := unix.Fchdir(newroot); err != nil {
+	if err := syscall.Fchdir(newroot); err != nil {
 		return fmt.Errorf("change to the new root failed, err: %v", err)
 	}
-	if err := unix.PivotRoot(".", "."); err != nil {
+	if err := syscall.PivotRoot(".", "."); err != nil {
 		return fmt.Errorf("pivot_root the new root failed, err: %v", err)
 	}
-	if err := unix.Fchdir(oldroot); err != nil {
+	if err := syscall.Fchdir(oldroot); err != nil {
 		return fmt.Errorf("change to the old root failed, err: %v", err)
 	}
 
-	//
-	if err := unix.Mount("", ".", "",
-		unix.MS_SLAVE|unix.MS_REC, ""); err != nil {
+	if err := syscall.Mount("", ".", "",
+		syscall.MS_SLAVE|syscall.MS_REC, ""); err != nil {
 		return fmt.Errorf("mount '.' failed, err: %v", err)
 	}
-	if err := unix.Unmount(".", unix.MNT_DETACH); err != nil {
+	if err := syscall.Unmount(".", syscall.MNT_DETACH); err != nil {
 		return fmt.Errorf("unmount '.' failed, err: %v", err)
 	}
 
-	if err := unix.Chdir("/"); err != nil {
+	if err := syscall.Chdir("/"); err != nil {
 		return fmt.Errorf("switch to new root failed, err: %v", err)
 	}
 	return nil
